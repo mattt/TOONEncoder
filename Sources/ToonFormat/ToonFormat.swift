7728152e13aa849c249f91f2fc5d3f3fd09bc687
alexey1312 <alexey1312ru@gmail.com>
2025-12-02 10:31:10 +0500

    Update file struct

---
+++ Sources/ToonFormat/ToonFormat.swift
@@ -0,0 +1,5 @@
+// ToonFormat - Umbrella module for TOON encoder and decoder
+// Re-exports both TOONEncoder and TOONDecoder for convenience
+
+@_exported import TOONDecoder
+@_exported import TOONEncoder
